e6f479c246d9bda8617eb7b1cb4d8d3af8a1aefb
jayaditya <jayadityar78@gmail.com>
2018-07-10 21:37:19 +0530

    Login.jsp added to views

--- investmentportfolio/src/main/java/net/jr/investmentportfolio/controller/PageController.java
+++ investmentportfolio/src/main/java/net/jr/investmentportfolio/controller/PageController.java
@@ -2,25 +2,32 @@ package net.jr.investmentportfolio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController 
 {
 	@RequestMapping(value= {"/","/home","/index"})
-	public ModelAndView index()
+	public ModelAndView dashboard()
 	{
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("hi" ,"hello J. Aditya");
+		ModelAndView mv = new ModelAndView("dashboard");
+//		mv.addObject("hi" ,"hello J. Aditya");
 		return mv;
 	}
 	
-	@RequestMapping(value= {"/test"})
+	/*@RequestMapping(value= {"/test"})
 	public ModelAndView test(@RequestParam(value="source",required=false) String req1 , @RequestParam(value="ei",required=true) String req2 )
 	{
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("hi" ,req2);
+		return mv;
+	}*/
+	
+	@RequestMapping(value= {"/login"})
+	public ModelAndView login()
+	{
+		ModelAndView mv = new ModelAndView("login");
+	//	mv.addObject("hi" ,req2);
 		return mv;
 	}
 }
